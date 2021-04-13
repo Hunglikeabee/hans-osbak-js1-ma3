c5ed056629e63f37830c22e23c06b7abcabca2b3
Hunglikeabee <74536958+Hunglikeabee@users.noreply.github.com>
2021-04-13 20:23:14 +0200

    Tiny fix
    Gotta fix the mistake :)

--- js/question-2.js
+++ js/question-2.js
@@ -1,18 +1,14 @@
 const containerDiv = document.querySelector(".containerDiv");
-const loading = document.querySelector(".loading");
-
-// const API_URL = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating"
 
 const API_URL = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 const CORS_URL = "https://noroffcors.herokuapp.com/";
 const CORS_API_FIX = CORS_URL + API_URL;
-
 const options = { "headers": {
     "x-rapidapi-key": "67f32ab14cmsh1b70c4982c0f023p1d66cfjsn8cc306174a31", 
 }};
 
 async function getTheApi() {
-        
+
     try {
         const response = await fetch(CORS_API_FIX, options);
         const result = await response.json();
@@ -31,7 +27,7 @@ async function getTheApi() {
                                         <h1>${theResults[i].name}</h1>
                                         <p>${theResults[i].rating}</p>
                                         <p>${theResults[i].tags.length}</p>
-                                      </div>`
+                                       </div>`
         }
 
     }
